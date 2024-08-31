b128268bf5f61f955bd3e03ede3b7c3a93da32cd
mohzim <mohzim@gmail.com>
2024-08-31 10:57:39 +0530

    Create multiple ec2 using for-each

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,25 @@
 resource "aws_instance" "nVX" {  
-    ami = "ami-0c2af51e265bd5e0e"
-    instance_type = "t3.micro"
-    key_name = "k8s-ubuntu-master-node"
+    ami = var.ec2_ami
+    instance_type = var.ec2_instance_type
+    key_name = var.ec2_key
 
     for_each = toset(["First", "Second"])
     tags = {
-        Name = "each.key Server"
+        Name = "${each.key} Server"
     }
 
+    # Sample code to use count instead of for-each.     
     #count = 2
     #tags = {
     #    Name = "nVX Web Server ${count.index+1}"
     #}
+    
+}
+
+output "ec2_liveip"{
+    value = [ for ec2 in aws_instance.nVX: ec2.public_ip]
+    
+    # Values in case of count used using aws_instance
+    #value = aws_instance.nVX[*].public_ip
     
 }
